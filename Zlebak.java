86ce8ba3e8c999c6dc76f0bca5ae07ab9a271086
nikostep1 <nikokladnik1@gmail.com>
2022-11-01 18:17:18 +0100

    Update Zlebak.java

--- Zlebak.java
+++ Zlebak.java
@@ -1,6 +1,6 @@
 public class Zlebak{
     public static void main(String args[]){
         System.out.println("Hello GitHub");
-         System.out.println("Hello GitHub second time");
+         System.out.println("Hello GitHub second time ok!");
     }
 }
